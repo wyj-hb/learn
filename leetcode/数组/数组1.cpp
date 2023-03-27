325f6550415941b4c8a7efb330edc3b112258297
Perter-Bit <1796755024@qq.com>
2023-03-27 17:11:43 +0800

    代码

---
+++ leetcode/数组/数组1.cpp
@@ -0,0 +1,114 @@
+﻿//#include<iostream>
+//using namespace std;
+//删除数组中的重复项，移动元素类型的题:
+//1.给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+////请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+//class Solution {
+//public:
+//    void moveZeroes(vector<int>& nums) {
+//        int length = 0;
+//        for (int i = 0;i < nums.size();i++)
+//        {
+//            if (nums[i] != 0)
+//            {
+//                length++;
+//            }
+//        }
+//        int j = 0;
+//        for (int i = 0;i < nums.size();i++)
+//        {
+//            if (nums[i])
+//            {
+//                nums[j] = nums[i];
+//                j++;
+//            }
+//        }
+//        for (int i = length;i < nums.size();i++)
+//        {
+//            nums[i] = 0;
+//        }
+//    }
+//};
+//2.给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+//不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+//元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+//class Solution {
+//public:
+//    int removeElement(vector<int>& nums, int val) {
+//        int fast = 0;
+//        int slow = 0;
+//        while (fast < nums.size()) {
+//            if (nums[fast] != val)
+//            {
+//                nums[slow] = nums[fast];
+//                slow++;
+//            }
+//            fast++;
+//        }
+//        return slow;
+//    }
+//};
+//3.给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+//由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么 nums 的前 k 个元素应该保存最终结果。
+//将最终结果插入 nums 的前 k 个位置后返回 k 。
+//不要使用额外的空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+//自己写:class Solution {
+//public:
+//    int removeDuplicates(vector<int>& nums) {
+//        int length = nums.size();
+//        int left = 0;
+//        int right = 0;
+//        int x = 0;
+//        bool panduan = false;
+//        while (right < length - 1)
+//        {
+//            while (nums[left] == nums[right] && right < length)
+//            {
+//                right++;
+//                if (right == length)
+//                {
+//                    panduan = true;
+//                    break;
+//                }
+//            }
+//            if (panduan)
+//            {
+//                length = left + 1;
+//                break;
+//            }
+//            int y = length - right;
+//            int z = right;
+//            for (int i = left;i < length && right < length;i++)
+//            {
+//                nums[i + 1] = nums[right];
+//                right++;
+//            }
+//            x++;
+//            length = length - z + left + 1;
+//优解:class Solution {
+//public:
+//    int removeDuplicates(vector<int>& nums) {
+//
+//        if (nums.size() == 0)
+//            return 0;
+//        int k = 0;
+//        for (int x : nums)
+//            if (x > nums[k])
+//                nums[++k] = x;
+//        return k + 1;
+//
+//    }
+//};
+//4.给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+//不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+/*class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int len = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (len < 2 || nums[i] != nums[len - 2])
+                nums[len++] = nums[i];
+        }
+        return len;
+    }
+};*///解决n重复问题
---
+++ leetcode/数组/数组2.cpp
@@ -0,0 +1,224 @@
+﻿//1.给定一个包含红色、白色和蓝色、共 n 个元素的数组 nums ，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+//我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+//必须在不使用库内置的 sort 函数的情况下解决这个问题。
+//使用冒泡排序:
+//class Solution {
+//public:
+//    void sortColors(vector<int>& nums) {
+//        for (int i = 0;i < nums.size() - 1;i++)
+//        {
+//            for (int j = 0;j < nums.size() - i - 1;j++)
+//            {
+//                if (nums[j] > nums[j + 1])
+//                {
+//                    int temp = nums[j + 1];
+//                    nums[j + 1] = nums[j];
+//                    nums[j] = temp;
+//                }
+//            }
+//        }
+//    }
+//};
+//优化算法，时间复杂度为大On,可保证left前的都是0，right后的都是2，二者之间的也就是1了
+//class Solution {
+//public:
+//    void sortColors(vector<int>& nums) {
+//        for (int i = 0;i < nums.size() - 1;i++)
+//        {
+//            for (int j = 0;j < nums.size() - i - 1;j++)
+//            {
+//                if (nums[j] > nums[j + 1])
+//                {
+//                    int temp = nums[j + 1];
+//                    nums[j + 1] = nums[j];
+//                    nums[j] = temp;
+//                }
+//            }
+//        }
+//    }
+//};
+//4.给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+//请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+//你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+//class Solution {
+//public:
+//    int findKthLargest(vector<int>& nums, int k) {
+        int max = nums[0];
+        int min = nums[0];
+        int x = nums.size();
+        for (int i = 1;i < nums.size();i++)
+        {
+            if (nums[i] >= max)
+            {
+                max = nums[i];
+            }
+            if (nums[i] <= min)
+            {
+                min = nums[i];
+            }
+        }
+        int length = max - min + 1;
+        vector<int> v1(x);
+        vector<int> v2(length, 0);
+        for (int i = 0;i < nums.size();i++)
+        {
+            v2[nums[i] - min]++;
+        }
+        v2[0]--;
+        for (int i = 1;i < length;i++)
+        {
+            v2[i] += v2[i - 1];
+        }
+        for (int i = nums.size() - 1;i >= 0;i--)
+        {
+            v1[v2[nums[i] - min]] = nums[i];
+            v2[nums[i] - min]--;
+        }
+        return v1[v1.size() - k];
+    }
+//};
+    class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+       //归并排序
+       if (m == 0 && n == 1)
+        {
+            nums1[0] = nums2[0];
+            return;
+        }
+       if (m == 1 && n == 0)
+        {
+             return;
+        }
+       int nums3[10000];
+       int nums4[10000];
+       for(int i = 0;i<m;i++)
+       {
+           nums3[i] = nums1[i];
+       }
+       for(int i = 0;i<n;i++)
+       {
+           nums4[i] = nums2[i];
+       }
+       nums3[m] = 10000;
+       nums4[n] = 10000;
+       int x = 0;
+       int y = 0;
+       for(int i = 0;i<m+n;i++)
+       {
+           if(nums3[x]>nums4[y])
+           {
+               nums1[i] = nums4[y];
+               y++;
+           }
+           else
+           {
+               nums1[i] = nums3[x];
+               x++;
+           }
+       }
+    }
+};
+//5.给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+//请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+//注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+// 方法一:  
+//  class Solution {
+//    public:
+//        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+//        {
+//            int x = 0;
+//            if (m == 0 && n == 1)
+//            {
+//                nums1[0] = nums2[0];
+//                return;
+//            }
+//            if (m == 1 && n == 0)
+//            {
+//                return;
+//            }
+//            for (int i = m;i < m + n;i++)
+//            {
+//                nums1[i] = nums2[x];
+//                x++;
+//            }
+//            int max = nums1[0];
+//            int min = nums1[0];
+//            for (int i = 1;i < m + n;i++)
+//            {
+//                if (nums1[i] >= max)
+//                {
+//                    max = nums1[i];
+//                }
+//                if (nums1[i] <= min)
+//                {
+//                    min = nums1[i];
+//                }
+//            }
+//            int length = max - min + 1;
+//            vector<int> v1(m + n);
+//            vector<int> v2(length, 0);
+//            for (int i = 0;i < nums1.size();i++)
+//            {
+//                v2[nums1[i] - min]++;
+//            }
+//            v2[0]--;
+//            for (int i = 1;i < length;i++)
+//            {
+//                v2[i] += v2[i - 1];
+//            }
+//            for (int i = nums1.size() - 1;i >= 0;i--)
+//            {
+//                v1[v2[nums1[i] - min]] = nums1[i];
+//                v2[nums1[i] - min]--;
+//            }
+//            for (int i = 0;i < m + n;i++)
+//            {
+//                nums1[i] = v1[i];
+//            }
+//        }
+//    };
+//方法二:class Solution {
+//public:
+//    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+//    {
+//        //归并排序
+//        if (m == 0 && n == 1)
+//        {
+//            nums1[0] = nums2[0];
+//            return;
+//        }
+//        if (m == 1 && n == 0)
+//        {
+//            return;
+//        }
+//        int nums3[10000];
+//        int nums4[10000];
+//        for (int i = 0;i < m;i++)
+//        {
+//            nums3[i] = nums1[i];
+//        }
+//        for (int i = 0;i < n;i++)
+//        {
+//            nums4[i] = nums2[i];
+//        }
+//        nums3[m] = 10000;
+//        nums4[n] = 10000;
+//        int x = 0;
+//        int y = 0;
+//        for (int i = 0;i < m + n;i++)
+//        {
+//            if (nums3[x] > nums4[y])
+//            {
+//                nums1[i] = nums4[y];
+//                y++;
+//            }
+//            else
+//            {
+//                nums1[i] = nums3[x];
+//                x++;
+//            }
+//        }
+//    }
+//};
